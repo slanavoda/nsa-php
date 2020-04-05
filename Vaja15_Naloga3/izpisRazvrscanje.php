97acac0d84f60777ae7fd3c7c7a4343971a66ebd
slanavoda <matej.skvarc@yandex.com>
2020-04-05 19:31:22 +0200

    vaja 15 naloga 3: popravek, manjka inner join

--- Vaja15_Naloga3/izpisRazvrscanje.php
+++ Vaja15_Naloga3/izpisRazvrscanje.php
@@ -3,13 +3,15 @@
         header('Location: izpisRazvrscanjeForm.php');
     }
 
+    echo '<style>table, th, td { border: 1px solid black; border-collapse: collapse; } </style>';
+
     $conn = mysqli_connect("localhost:3306", "root", "", "baza1") or die("Napaka pri povezavi s strežnikom");
     if (!isset($_POST['sort'])) {
-        $query = "select * from oseba";
+        $query = "select KID, ime, priimek, rojstvo from oseba";
     } else if (count($POST['sort']) == 1) {
-        $query = "select * from oseba order by ".$_POST['sort']." ".$_POST['s'];
+        $query = "select KID, ime, priimek, rojstvo from oseba order by ".$_POST['sort']." ".$_POST['s'];
     } else {
-        $query = "select * from oseba order by ";
+        $query = "select KID, ime, priimek, rojstvo from oseba order by ";
         foreach ($_POST['sort'] as $k => $value) {
             if ($k == 0) {
                 $query = $query.$value;
