d0eec4b1dfdc0e9b1dd6751211fb8918eec04fae
slanavoda <matej.skvarc@yandex.com>
2020-04-06 11:32:30 +0200

    vaja 17 naloga 1: blokiranje uporabnika za 1 uro po 5 poskusih

--- Vaja17_Naloga1/prijava.php
+++ Vaja17_Naloga1/prijava.php
@@ -4,7 +4,7 @@
     }
 
     $conn = mysqli_connect("localhost:3306", "root", "", "geografija1") or die("Napaka pri povezavi s strežnikom");
-    $query = "select geslo, stNeveljavnihPoskusov from Uporabnik where uIme = '".$_POST['uIme']."'";
+    $query = "select geslo, stNeveljavnihPoskusov, datumCasPrijave from Uporabnik where uIme = '".$_POST['uIme']."'";
     $rs = mysqli_query($conn, $query) or die("Napaka pri poizvedbi!");
 
     if (mysqli_num_rows($rs) == 0) {
@@ -13,6 +13,19 @@
 
     $line = mysqli_fetch_assoc($rs);
     
+    if ($line['stNeveljavnihPoskusov'] >= 5) {
+        $datumCas1 = date_create($line['datumCasPrijave']);
+        $datumCas2 = date_create(date("Y-m-d h:i:s"));
+        $razlika = date_diff($datumCas1, $datumCas2);
+        if (($razlika->h) < 1) {
+            $timespan = 3600;
+            $dv = new DateInterval('PT'.$timespan.'S');
+            $datumCas3 = date_add($datumCas1, $dv);
+            $razlika2 = date_diff($datumCas2, $datumCas3);
+            die("Ne moreš se prijaviti še ".$razlika2->i." minut.<br><a href='prijavaForm.php'>Domov</a>");
+        }
+    }
+        
     if (password_verify($_POST['geslo'], $line['geslo'])) {
         $query = "update uporabnik set stNeveljavnihPoskusov=0, datumCasPrijave='".date("Y-m-d h:i:s")."' where uIme='".$_POST['uIme']."'";
         mysqli_query($conn, $query) or die("Napaka pri poizvedbi 2!");
@@ -23,7 +36,7 @@
         header("Location: index.php");
     } else {
         $st = ($line['stNeveljavnihPoskusov'] * 1) + 1;
-        $query = "update uporabnik set stNeveljavnihPoskusov=".$st." where uIme='".$_POST['uIme']."'";
+        $query = "update uporabnik set stNeveljavnihPoskusov=".$st.", datumCasPrijave='".date("Y-m-d h:i:s")."' where uIme='".$_POST['uIme']."'";
         mysqli_query($conn, $query) or die("Napaka pri poizvedbi 2!");
         mysqli_close($conn);
         header("Location: prijavaForm.php");
