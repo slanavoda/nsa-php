10309479d53da1145738d9546133f8f56e78033d
slanavoda <matej.skvarc@yandex.com>
2020-04-06 11:10:40 +0200

    vaja 17 naloga 1: odstrani nepotrebno zanko

--- Vaja17_Naloga1/prijava.php
+++ Vaja17_Naloga1/prijava.php
@@ -11,21 +11,21 @@
         header("Location: prijavaForm.php");
     }
 
-    while ($line = mysqli_fetch_assoc($rs)) {
-        if (password_verify($_POST['geslo'], $line['geslo'])) {
-            $query = "update uporabnik set stNeveljavnihPoskusov=0, datumCasPrijave='".date("Y-m-d h:i:s")."' where uIme='".$_POST['uIme']."'";
-            mysqli_query($conn, $query) or die("Napaka pri poizvedbi 2!");
-            mysqli_close($conn);
-            session_start();
-            $_SESSION['uIme'] = $_POST['uIme'];
-            $_SESSION['login'] = "OK";
-            header("Location: index.php");
-        } else {
-            $st = ($line['stNeveljavnihPoskusov'] * 1) + 1;
-            $query = "update uporabnik set stNeveljavnihPoskusov=".$st." where uIme='".$_POST['uIme']."'";
-            mysqli_query($conn, $query) or die("Napaka pri poizvedbi 2!");
-            mysqli_close($conn);
-            header("Location: prijavaForm.php");
-        }
+    $line = mysqli_fetch_assoc($rs);
+    
+    if (password_verify($_POST['geslo'], $line['geslo'])) {
+        $query = "update uporabnik set stNeveljavnihPoskusov=0, datumCasPrijave='".date("Y-m-d h:i:s")."' where uIme='".$_POST['uIme']."'";
+        mysqli_query($conn, $query) or die("Napaka pri poizvedbi 2!");
+        mysqli_close($conn);
+        session_start();
+        $_SESSION['uIme'] = $_POST['uIme'];
+        $_SESSION['login'] = "OK";
+        header("Location: index.php");
+    } else {
+        $st = ($line['stNeveljavnihPoskusov'] * 1) + 1;
+        $query = "update uporabnik set stNeveljavnihPoskusov=".$st." where uIme='".$_POST['uIme']."'";
+        mysqli_query($conn, $query) or die("Napaka pri poizvedbi 2!");
+        mysqli_close($conn);
+        header("Location: prijavaForm.php");
     }
 ?>
