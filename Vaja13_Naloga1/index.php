43201913bfae384ca8ddb3bd3d24a561acb2eb1d
slanavoda <matej.skvarc@yandex.com>
2020-04-06 12:12:19 +0200

    vaja 13 naloga 1: dodaj fieldset

--- Vaja13_Naloga1/index.php
+++ Vaja13_Naloga1/index.php
@@ -4,18 +4,21 @@
     }
 ?>
 <form action="ugibaj.php" method="post">
-    <input type="number" min="1" max="100" name="ugib" placeholder="Ugibaj!"><br>
-    <?php
-        if ($_COOKIE['x'] == '0') {
-            echo '<input type="text" value="Število ste zadeli v '.$_COOKIE['i'].' poskusih!" disabled><br>';
-        } else if ($_COOKIE['x'] == '1') {
-            echo '<input type="text" value="Iskano število je manjše!" disabled><br>';
-        } else if ($_COOKIE['x'] == '2') {
-            echo '<input type="text" value="Iskano število je večje!" disabled><br>';
-        } else if ($_COOKIE['x'] == '3') {
-            echo '<input type="text" value="" disabled><br>';
-        }
-    ?>
-    <input type="submit" value="Ugibaj!">
-    <input type="submit" value="Nova Igra" name="nova">
+    <fieldset style="display: inline-block">
+        <legend>Ugibaj število!</legend>
+        <input type="number" min="1" max="100" name="ugib" placeholder="Ugibaj!"><br>
+        <?php
+            if ($_COOKIE['x'] == '0') {
+                echo '<input type="text" value="Število ste zadeli v '.$_COOKIE['i'].' poskusih!" disabled><br>';
+            } else if ($_COOKIE['x'] == '1') {
+                echo '<input type="text" value="Iskano število je manjše!" disabled><br>';
+            } else if ($_COOKIE['x'] == '2') {
+                echo '<input type="text" value="Iskano število je večje!" disabled><br>';
+            } else if ($_COOKIE['x'] == '3') {
+                echo '<input type="text" value="" disabled><br>';
+            }
+        ?>
+        <input type="submit" value="Ugibaj!">
+        <input type="submit" value="Nova Igra" name="nova">
+    </fieldset>
 </form>
